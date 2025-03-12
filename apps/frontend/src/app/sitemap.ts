aac1e4f00c04b034f7740df1f551c5e0aec5bff1
set <lucameopou@gmail.com>
2025-03-12 20:40:54 +0100

    feat(sitemap): implement sitemap generation for improved SEO and site indexing

---
+++ apps/frontend/src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/local-multiplayer`,
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.8,
+    },
+  ]
+}
