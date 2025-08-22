4186b41fb013b6890cdfe664f9cd79873319be11
set <lucameopou@gmail.com>
2025-08-22 16:05:00 +0200

    feat: add /how-to-play URL to sitemap

--- apps/frontend/src/app/sitemap.ts
+++ apps/frontend/src/app/sitemap.ts
@@ -8,6 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			priority: 1,
 		},
 		{
+			url: `${process.env.NEXT_PUBLIC_APP_URL}/how-to-play`,
+			lastModified: new Date(),
+			priority: 0.9,
+		},
+		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/local-multiplayer`,
 			lastModified: new Date(),
 			priority: 0.8,
