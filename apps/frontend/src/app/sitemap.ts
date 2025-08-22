b1d41d3956f8f7874a600d5b8a3cf9b95238fdb8
set <lucameopou@gmail.com>
2025-08-22 15:44:12 +0200

    refactor: remove change frequency from sitemap

--- apps/frontend/src/app/sitemap.ts
+++ apps/frontend/src/app/sitemap.ts
@@ -5,13 +5,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}`,
 			lastModified: new Date(),
-			changeFrequency: "monthly",
 			priority: 1,
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/local-multiplayer`,
 			lastModified: new Date(),
-			changeFrequency: "always",
 			priority: 0.8,
 		},
 	];
